e58269ee824eec5077443f4c69641684dd9ac272
Veselin Kolev <vesso.kolev@gmail.com>
2018-09-06 21:01:02 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,57 @@
+# parser.py - lightweight parser for locating entries in the FreeRADIUS log files by MAC address
+
+## General information
+
+The goal of developing and supporting this parser script, is to help analyzing the log files mainainned by the [FreeRADIUS](https://freeradius.org/) daemon `radiusd` (the content of those files is in plain text). When analyzing those files, the script is parsing their content line by line, and conditionally performing an additional check, if the line content has pattern mathching the one typical for the WiFi clients' autnetnication requests. That additional check has a goal to check if the MAC address of the client's WiFi adaper matches the one passed to the script as an invoking parameter. If match is found, the script prints the line to the standard output. 
+
+The script might be of help to the administrators of the FreeRADIUS servers that are part of the [Eduroam](https://www.eduroam.org) infrastructure. Its code could be modified to support specific tasks like grouping the entries in the log files or providing front end to Zabbix agents.
+
+## Information regarding the format of MAC addresses reported to the log files
+
+The MAC address of the client's WiFi adapter, along with some information regarding the process of authentication, is reported to `/var/log/radius/radius.log`. There, the string representing the MAC address number, can match one of the following formats:
+```
+0123456789ab
+01:23:45:67:89:ab
+01-23-45-67-89-ab
+0123-4567-89ab
+0123.4567.89ab
+```
+Those formats are imposed by the type of the reporting access point device. Different WiFi access point devices vendors adopts differen format for reporting the MAC address of the client to the RADIUS server.
+
+The script code has the ability to parse the log files line by line, to recognize there the sought MAC address, regardless its string format, and print that line out to the standard output.
+
+## Pre-requsites
+
+To execute the script one needs to have installed the recent [Python](http://python.org) 2.7 or 3.x.
+
+## Download the code
+
+$ git clone https://github.com/vessokolev/radius_log_parser.git
+
+## License
+
+See the file LICENSE.
+
+## Author
+
+See the file AUTHORS.
+
+## Examples:
+
+Search for any entries, related to the authentication of a client with MAC address `0123456789ab` and reported to `/var/log/radius/radius.log` (the default `radiusd` log file):
+
+```
+# parser.py 0123456789ab
+```
+
+Search for any entries, related to the authentication of a client with MAC address `0123456789ab` and reported to `/var/log/radius/radius.log-20180901.gz`:
+
+```
+# parser.py 0123456789ab /var/log/radius.log-20180901.gz
+```
+
+Search for any entries, related to the authentication of a client with MAC address `0123456789ab` and reported to any of the `radiusd` log files stored in `/var/log/radius/`:
+
+```
+# FILES=`ls /var/log/radius/radius.log*` && for i in $FILES ; do parser.py 0123456789ab $i ; done
+```
---
+++ parser.py
@@ -0,0 +1,135 @@
+#!/usr/bin/env python
+
+
+__author__ = "Veselin Kolev"
+__license__ = "GPLv2"
+__version__ = "2018090500"
+__maintainer__ = __author__
+__email__ = "vesso.kolev@gmail.com"
+__status__ = "Production"
+__about__ = '''This program code parses FreeRADIUS radius.log files and,
+prints out those of the lines there that contains certain
+MAC address. It also supports Gzip-ed radius.log files.'''
+
+
+def check_mac(string_to_check):
+
+    '''This function verifies if the input string represents a MAC address.'''
+
+    digits = '0123456789ABCDEF' # The hexidecimal (HEX) digits.
+
+    MAC = ''.join([i for i in string_to_check.upper() if i in digits])
+
+    if len(MAC) != 12: # Every MAC address is 12-digit HEX number.
+                       # Return None if the string length does not
+                       # match the one expected.
+
+        return None
+
+    else:
+
+        return MAC     # The MAC address digits are all upper case.
+
+
+def check_input(sys):
+
+    if len(sys.argv) < 2:
+
+        print('\n****** ERROR ******')
+
+        print('\nInsufficient number of input parameters! Invoke the script as:\n')
+
+        print(sys.argv[0],'MAC_ADDRESS (file)\n')
+
+        print('where MAC_ADDRESS is 12-digit integer number representing a MAC address.' + \
+              ' For example: AC128A35BB0D.\n')
+
+        print('   * Optionally, one can specify the RADIUS log file, in FreeRADUIS log ' + \
+              'format. Gzip files are also supported.\n')
+
+        sys.exit(1)
+
+    return None
+
+
+def parse_file(sys, radiusd_log):
+
+    use_gzip = False
+
+    if len(sys.argv) == 3:
+
+        radiusd_log = sys.argv[2]
+
+        if radiusd_log[-2:].lower() == 'gz':
+
+            use_gzip = True
+
+            import gzip
+
+            # Note that setting an encoding is not supported in binary mode in
+            # 'open'!
+
+            open_file = gzip.open(radiusd_log, 'rb',)
+
+        else:
+
+            open_file = open(radiusd_log, 'r', encoding='utf-8', errors='replace')
+
+    else:
+
+        open_file = open(radiusd_log, 'r', encoding='utf-8', errors='replace')
+
+
+    with open_file as f_obj:
+
+        for line in f_obj:
+
+            if use_gzip:
+
+                # Handle specifically the Gzip-ed files:
+
+                line = line.decode(encoding='utf-8', errors='replace')
+
+            if 'Auth:' in line and ' cli ' in line:
+
+                tmp = line.split(' cli ')[1].split(')')[0]
+                tmp = check_mac(tmp)
+
+                if sys.argv[1] == tmp:
+
+                    print(line)
+
+
+if __name__ == '__main__':
+
+    import sys
+
+    # The default location and of the FreeRADIUS log file.
+    # The default in CentOS / RHEL / Scientific Linux is:
+    #
+    # /var/log/radius/radius.log
+    #
+    # Change it accordingly, if you use different Linux distribution.
+    #
+    # Ths script also supports parsing Gzip-ed files.
+
+    radiusd_log = '/var/log/radius/radius.log'
+
+    check_input(sys)
+
+    # Cannonize the input string to turn it into MAC:
+
+    inp_MAC = check_mac(sys.argv[1])
+
+    if inp_MAC is None:
+
+        print('\nERROR!!!')
+        print('\nThe input string:\n\n',sys.argv[1],'\n\ndoes not represent any valid MAC address!\n')
+
+        sys.exit(1)
+
+    else:
+
+        sys.argv[1] = inp_MAC
+
+    parse_file(sys, radiusd_log)
